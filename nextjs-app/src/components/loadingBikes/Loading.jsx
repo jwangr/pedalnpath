b99e58818cf7305608c60a6be4b4045df65ec791
ThinkCentre <jwangr@ymail.com>
2025-09-28 08:52:23 +1000

    "Added backdrop"

--- nextjs-app/src/components/loadingBikes/Loading.jsx
+++ nextjs-app/src/components/loadingBikes/Loading.jsx
@@ -1,10 +1,14 @@
+import { Backdrop } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
 export default function Loading() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-bounce w-50 h-50 z-[1000]">
-      <Image src="/24356-673ab7.svg" fill alt="loading" />
-    </div>
+    <>
+      <Backdrop sx={{ color: "#bfbfbfff", zIndex: 20 }}></Backdrop>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-bounce w-50 h-50 z-[1000]">
+        <Image src="/24356-673ab7.svg" fill alt="loading" />
+      </div>
+    </>
   );
 }
