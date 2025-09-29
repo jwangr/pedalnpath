dedc451c7486da11d7b621f3726bb6fb7697fc20
ThinkCentre <jwangr@ymail.com>
2025-09-29 10:35:52 +1000

    updated Loading component with backdrop

--- nextjs-app/src/components/loadingBikes/Loading.jsx
+++ nextjs-app/src/components/loadingBikes/Loading.jsx
@@ -5,8 +5,11 @@ import React from "react";
 export default function Loading() {
   return (
     <>
-      <Backdrop sx={{ color: "#bfbfbfff", zIndex: 20 }}></Backdrop>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-bounce w-50 h-50 z-[1000]">
+      <Backdrop
+        open={true}
+        sx={{ color: "#bfbfbfff", zIndex: (theme) => theme.zIndex.modal + 1 }}
+      ></Backdrop>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-bounce w-50 h-50 z-[2000]">
         <Image src="/24356-673ab7.svg" fill alt="loading" />
       </div>
     </>
