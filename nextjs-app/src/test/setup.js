c0aac0139d380ef3603c79029bd3a344b956ea98
ThinkCentre <jwangr@ymail.com>
2025-10-02 20:17:11 +1000

    Need to change syntax to use testing

--- nextjs-app/src/test/setup.js
+++ nextjs-app/src/test/setup.js
@@ -1,17 +1,17 @@
-import moduleAlias from 'module-alias';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+// import moduleAlias from 'module-alias';
+// import { fileURLToPath } from 'url';
+// import { dirname, resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-// Register aliases
-moduleAlias.addAliases({
-  '@': resolve(__dirname, '../'),
-  '@test': resolve(__dirname, '../test'),
-  '@dao': resolve(__dirname, '../lib/dao'),
-  '@controllers': resolve(__dirname, '../lib/controllers')
-});
+// // Register aliases
+// moduleAlias.addAliases({
+//   '@': resolve(__dirname, '../'),
+//   '@test': resolve(__dirname, '../test'),
+//   '@dao': resolve(__dirname, '../lib/dao'),
+//   '@controllers': resolve(__dirname, '../lib/controllers')
+// });
 
 // Test setup
 process.env.NODE_ENV = 'test';
