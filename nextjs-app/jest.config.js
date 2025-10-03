9c85e17c5538a8f26cc8d1cf463214843e85e479
ThinkCentre <jwangr@ymail.com>
2025-10-03 19:26:27 +1000

    Set up jest configuration with https://www.wisp.blog/blog/how-to-use-jest-with-nextjs-15-a-comprehensive-guide

---
+++ nextjs-app/jest.config.js
@@ -0,0 +1,30 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./", // Path to nextjs-app
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    // Handle module aliases (if you use them in your app) - can try to generalise with: '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/app/(.*)$": "<rootDir>/src/app/$1",
+    "^@/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/generated/(.*)$": "<rootDir>/src/generated/$1",
+    "^@/lib/(.*)$": "<rootDir>/src/lib/$1",
+    "^@/services/(.*)$": "<rootDir>/src/services/$1",
+    "^@/store/(.*)$": "<rootDir>/src/store/$1",
+  },
+
+  //   or handling ES Modules
+  transformIgnorePatterns: ["/node_modules/(?!(@your-problematic-module)/)"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+module.exports = createJestConfig(customJestConfig);
