8fde010aeaeeb1f51fbec5f39d39631c02a5aaff
ThinkCentre <jwangr@ymail.com>
2025-10-03 19:55:33 +1000

    Changed to ESM syntax

--- nextjs-app/jest.config.js
+++ nextjs-app/jest.config.js
@@ -1,4 +1,4 @@
-const nextJest = require("next/jest");
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
   dir: "./", // Path to nextjs-app
@@ -27,4 +27,4 @@ const customJestConfig = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
 
-module.exports = createJestConfig(customJestConfig);
+export default createJestConfig(customJestConfig);
