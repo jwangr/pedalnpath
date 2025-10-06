8d08909dc25c39c246987ebeb4f82b2707383bbd
ThinkCentre <jwangr@ymail.com>
2025-10-06 19:39:41 +1100

    Test for deleting and creating bike paths

--- nextjs-app/__tests__/controllers/BikePathDBController.test.js
+++ nextjs-app/__tests__/controllers/BikePathDBController.test.js
@@ -1,14 +1,26 @@
 import BikePathDBController from "@/lib/controllers/BikePathDBController";
 import NotFoundError from "@/lib/utils/errors/NotFoundError";
 import { mockBikePathDao } from "../dao/mockBikePathDao";
+import ValidateNewBikePath from "@/lib/utils/validation/ValidateNewBikePath";
+
+// mock the validate path function (will be separately unit tested)
+jest.mock("../../src/lib/utils/validation/ValidateNewBikePath.js", () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      validatePath: jest.fn(),
+    };
+  });
+});
 
 describe("BikePath controller", () => {
   let controller;
+  let mockValidatorInstance;
 
   beforeEach(() => {
     // Reset mocks
     jest.clearAllMocks();
     controller = new BikePathDBController(mockBikePathDao);
+    mockValidatorInstance = new ValidateNewBikePath();
   });
 
   it("returns all bike paths", async () => {
@@ -75,5 +87,31 @@ describe("BikePath controller", () => {
     expect(mockBikePathDao.getAllPaths).toHaveBeenCalledTimes(1);
     expect(mockBikePathDao.getAllPaths).toHaveBeenCalledWith(mockUserId);
     expect(result).toEqual(mockPaths);
+  });
+
+  it("calls validator and creates Path()", async () => {
+    const fakePath = { title: "River Trail" };
+    const parsedPath = { ...fakePath, id: 1 };
+
+    const req = { json: jest.fn().mockResolvedValue(fakePath) };
+    mockValidatorInstance.validatePath.mockReturnValue(parsedPath);
+    mockBikePathDao.createPath.mockResolvedValue(parsedPath);
+
+    const result = await controller.createPath(req);
+    expect(req.json).toHaveBeenCalled();
+    // expect(mockValidatorInstance.validatePath).toHaveBeenCalledTimes(1);
+    expect(mockBikePathDao.createPath).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(parsedPath);
+  });
+
+  it("calls dao.deletePath with correct id", async () => {
+    const req = { url: "http://localhost:3000/api/bikepaths?id=42" };
+
+    mockBikePathDao.deletePath.mockResolvedValue({ success: true });
+
+    const result = await controller.deletePath(req);
+
+    expect(mockBikePathDao.deletePath).toHaveBeenCalledWith(42);
+    expect(result).toEqual({ success: true });
   });
 });
--- nextjs-app/__tests__/dao/mockBikePathDao.js
+++ nextjs-app/__tests__/dao/mockBikePathDao.js
@@ -1,4 +1,6 @@
 export const mockBikePathDao = {
   getAllPaths: jest.fn(),
   findPathByName: jest.fn(),
+  createPath: jest.fn(),
+  deletePath: jest.fn(),
 };
