fab7746d7a6a50b298effc8e42aa41e99e45a638
ThinkCentre <jwangr@ymail.com>
2025-10-08 14:42:52 +1100

    Checks for valid Gemini responses

--- nextjs-app/__tests__/validation/validateGeminiResponse.test.js
+++ nextjs-app/__tests__/validation/validateGeminiResponse.test.js
@@ -3,8 +3,120 @@ import validateGeminiResponse, {
 } from "@/lib/utils/validation/validateGeminiResponse";
 
 describe("Validates of Gemini Responses", () => {
-  it("Throws an error message if response is invalid JSON", () => {
+  it("parses and returns valid json - must include title, startCoordinate, endCoordinate, coordinates []", () => {
+    let validData = JSON.stringify([
+      {
+        id: 1,
+        title: "Title 1",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+      {
+        id: 2,
+        title: "Title 2",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+      {
+        id: 3,
+        title: "Title 3",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+    ]);
+    expect(validateGeminiResponse(validData)).toEqual(JSON.parse(validData));
+  });
+
+  it("throws an error message if response is invalid JSON", () => {
     let mockedData = "invalid json {hehe: invalid}";
-    expect(() => validateGeminiResponse(mockedData)).toThrow("Gemini response is not valid JSON");
+    expect(() => validateGeminiResponse(mockedData)).toThrow(
+      "Gemini response is not valid JSON"
+    );
+  });
+
+  it("Throws an error if JSON objects is not in an array", () => {
+    let validData = JSON.stringify({
+      title: "Title 1",
+      startCoordinate: 0,
+      // endCoordinate: 0,
+      coordinates: [0, 0],
+    });
+    expect(() => validateGeminiResponse(validData)).toThrow(
+      "Gemini response should be an array"
+    );
+  });
+
+  it("Throws an error if JSON objects doesn't include coordinates array", () => {
+    let validData = JSON.stringify([
+      {
+        id: 1,
+        title: "Title 1",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        // coordinates: [0, 0],
+      },
+      {
+        id: 2,
+        title: "Title 2",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        // coordinates: [0, 0],
+      },
+      {
+        id: 3,
+        title: "Title 3",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        // coordinates: [0, 0],
+      },
+    ]);
+    expect(() => validateGeminiResponse(validData)).toThrow(
+      `Missing key \"coordinates\" in Gemini response item`
+    );
+  });
+
+  it("Throws an error if JSON objects doesn't include title", () => {
+    let validData = JSON.stringify([
+      {
+        // title: "Title 1",
+        startCoordinate: 0,
+        endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+    ]);
+    expect(() => validateGeminiResponse(validData)).toThrow(
+      `Missing key \"title\" in Gemini response item`
+    );
+  });
+
+  it("Throws an error if JSON objects doesn't include startCoordinate", () => {
+    let validData = JSON.stringify([
+      {
+        title: "Title 1",
+        // startCoordinate: 0,
+        endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+    ]);
+    expect(() => validateGeminiResponse(validData)).toThrow(
+      `Missing key \"startCoordinate\" in Gemini response item`
+    );
+  });
+
+  it("Throws an error if JSON objects doesn't include endCoordinate", () => {
+    let validData = JSON.stringify([
+      {
+        title: "Title 1",
+        startCoordinate: 0,
+        // endCoordinate: 0,
+        coordinates: [0, 0],
+      },
+    ]);
+    expect(() => validateGeminiResponse(validData)).toThrow(
+      `Missing key \"endCoordinate\" in Gemini response item`
+    );
   });
 });
